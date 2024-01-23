35d4c1e681752a4012f8a64abfc6db5efd5ae834
JaKa69 <alix.babin@gmail.com>
2024-01-23 20:21:04 +0100

    migration css vers scss

--- src/app/component/auth-content/auth-content.component.ts
+++ src/app/component/auth-content/auth-content.component.ts
@@ -4,7 +4,7 @@ import { AxiosService } from 'src/app/service/axios.service';
 @Component({
   selector: 'app-auth-content',
   templateUrl: './auth-content.component.html',
-  styleUrls: ['./auth-content.component.css']
+  styleUrls: ['./auth-content.component.scss']
 })
 export class AuthContentComponent implements OnInit{
   data:string[] = [];
