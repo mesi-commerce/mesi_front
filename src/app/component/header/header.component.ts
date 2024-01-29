dd1fe4e92a0a591175c542e00e6d4ebfe49ae42f
Mathias <mathias.suchail@yahoo.com>
2024-01-29 14:40:03 +0100

    Ajout bootstrap + header ok

--- src/app/component/header/header.component.ts
+++ src/app/component/header/header.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+  protected isDiscountClosed:boolean;
+  constructor() {
+    this.isDiscountClosed = false;
+  }
 
+  public closeDiscount(){
+    this.isDiscountClosed=true
+  }
 }
